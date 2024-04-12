4624485dd6fe58bd1d5ac73af5f4de8d70a33bd2
Rebekah <macrosystem@proton.me>
2024-04-13 01:52:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+#FIX ME
+def get_list():
+  output_list = ["cat", "dog", "parrot", "red", "green", "blue"]
+  return output_list
+
+def randomize_list(a):
+  a = a #FIX ME: randomize
+  return a
+  #is it better to scrable list or rang a number and check if its already been genereated?
+
+#FIX ME: pull apart, 
+def get_result():
+  count = 0
+  output_list = []
+  for item in randomize_list(get_list()):
+    output_list.append(f"{count} : {item}")
+    count += 1
+  return output_list
+  
+def print_result():
+  for item in get_result():
+    print(item)
+
+print_result()
