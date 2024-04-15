0a5a5cfe1db0be760a9695cef5c42494ca518236
Rebekah <macrosystem@proton.me>
2024-04-16 00:08:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ def get_list(file_path):
     return output_list
 
 def randomize_list(a):
-  return random.sample(a)
+    return random.sample(a)
 
 #FIX ME: pull apart, 
 def get_result():
-  count = 0
-  output_list = []
-  for item in randomize_list(get_list(nouns.txt)): #FIX ME: input for filepath/standard path if no output (like wordlist.txt)
-    output_list.append(f"{count}, {item}")
-    count += 1
-  return output_list
+    count = 0
+    output_list = []
+    for item in randomize_list(get_list(nouns.txt)): #FIX ME: input for filepath/standard path if no output (like wordlist.txt)
+        output_list.append(f"{count}, {item}")
+        count += 1
+    return output_list
   
 def print_result():
-  for item in get_result():
-    print(item)
+    for item in get_result():
+        print(item)
+
+#fix me
+def save_results(output_data = "default"):
+    with open('output.csv', 'w') as my_file:
+        my_file.write(output_data)
 
 print_result()
